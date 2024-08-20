1519af2971ac6bb8134f42f0b2a68d83dcf5b790
shubhanshu <shubhanshukrssm10@gmail.com>
2024-08-20 19:20:25 +0530

    second

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 document.querySelector(".icon").onclick= ()=>{
     // console.log("hii");
     document.querySelector("ul").style.left = 0
-    document.querySelector(".fa-close").classList.add("active")
+    // document.querySelector(".fa-close").classList.add("active")
     document.querySelector(".fa-close").style.display = "block"
    
 }
@@ -9,6 +9,6 @@ document.querySelector(".icon").onclick= ()=>{
 document.querySelector(".fa-close").onclick= ()=>{
     // console.log("hello");
     document.querySelector("ul").style.left = "100%"
-    document.querySelector(".fa-close").classList.remove("active")
+    // document.querySelector(".fa-close").classList.remove("active")
     document.querySelector(".fa-close").style.display = "none"
 }
